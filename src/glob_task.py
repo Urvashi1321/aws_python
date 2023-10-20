f359af5b7ccc343e97c8e8cab0d06efc4cb8ced6
Urvashi Dagar <dagarurvashi1321@gmail.com>
2023-10-20 15:50:26 +0530

    Glob module

--- src/glob_task.py
+++ src/glob_task.py
@@ -1,6 +1,17 @@
-import fnmatch
+import glob
 import os
+#Directory where files are to be searched
+directory = 'C:/Users/7000033086/Documents/aws_python/src'
 
-for file in os.scandir("."):
-    if fnmatch.fnmatch(file, "*.py"):
-        print('Filename : ',file, fnmatch.fnmatch(file, "*.py"))
+#File patterns that are to be searched
+file_patterns = ['*.py', '*.txt']
+
+#Iterate through the file patterns and search for files
+for pattern in file_patterns:
+    #glob.glob function to find files that match the pattern
+    files = glob.glob(f"{directory}/{pattern}")
+
+    #Print the list of files found for the current pattern
+    print(f"Files matching pattern '{pattern}':")
+    for file in files:
+        print(file)
